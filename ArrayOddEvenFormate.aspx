f43474c2465a3db074795ff05c72aa6c2b87bd9f
Suraj Rajendra Pardeshi <152958796+SurajPardeshi636@users.noreply.github.com>
2023-12-12 09:19:11 +0530

    Add files via upload
    Take one array 100 
    next dropdown sepration of even num and third dropdown odd num

---
+++ ArrayOddEvenFormate.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ArrayOddEvenFormate.aspx.cs" Inherits="ArrayTask08Dec.ArrayOddEvenFormate" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 100px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h3>Array Even & Odd</h3>
+        </div>
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style2">Array List:</td>
+                <td>
+                    <asp:DropDownList ID="DrpArrayList" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DrpArrayList_SelectedIndexChanged">
+                    </asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">EvenArray:</td>
+                <td>
+                    <asp:DropDownList ID="DrpEven" runat="server">
+                    </asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">OddArray:</td>
+                <td>
+                    <asp:DropDownList ID="DrpOdd" runat="server">
+                    </asp:DropDownList>
+                </td>
+            </tr>
+        </table>
+    </form>
+</body>
+</html>
---
+++ ArrayOddEvenFormate.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ArrayTask08Dec
+{
+    public partial class ArrayOddEvenFormate : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+
+
+        {
+            if (!IsPostBack)
+            {
+               
+                BindArrayList();
+            }
+        }
+
+        private void BindArrayList()
+        {
+            List<int> numbers = new List<int>();
+
+            for (int i = 1; i <= 100; i++)
+            {
+                numbers.Add(i);
+            }
+
+            DrpArrayList.DataSource = numbers;
+            DrpArrayList.DataBind();
+        }
+
+        protected void DrpArrayList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            //int selectedValue = Convert.ToInt32(DrpArrayList.SelectedValue);
+
+            
+            BindDropDownList2();
+
+            
+            BindDropDownList3();
+        }
+
+        private void BindDropDownList2()
+        {
+            List<int> evenNumbers = new List<int>();
+
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    evenNumbers.Add(i);
+                }
+            }
+
+            DrpEven.DataSource = evenNumbers;
+            DrpEven.DataBind();
+        }
+
+        private void BindDropDownList3()
+        {
+            List<int> oddNumbers = new List<int>();
+
+            for (int i = 0; i <= 100; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    oddNumbers.Add(i);
+                }
+            }
+
+            DrpOdd.DataSource = oddNumbers;
+            DrpOdd.DataBind();
+        }
+        
+    }
+}
+
+//int[] numArray = new int[100];
+
+//for (int i = 0; i< 100; i++)
+//{
+//    numArray[i] = i + 1;
+//}
